7c4e8d9e43f903da6be466caedbc4db464430f77
Elisa Boscani <elisabosc90@gmail.com>
2023-09-06 15:12:32 +0200

    created the array containing the team information

---
+++ asset/js/script.js
@@ -0,0 +1,47 @@
+/* 
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, 
+le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe */
+
+const teams = [
+  {
+    name: "Wayne",
+    lastName: "Barnett",
+    job: "Founder & CEO",
+    img: "./asset/img/wayne-barnett-founder.jpg",
+  },
+  {
+    name: "Angela",
+    lastName: "Caroll",
+    job: "Chief Editor",
+    img: "./asset/img/angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter",
+    lastName: "Gordon",
+    job: "Office Manager",
+    img: "./asset/img/walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela",
+    lastName: "Lopez",
+    job: "Social Media Manager",
+    img: "./asset/img/angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott",
+    lastName: "Estrada",
+    job: "Developer",
+    img: "./asset/img/scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara",
+    lastName: "Ramos",
+    job: "Designer",
+    img: "./asset/img/barbara-ramos-graphic-designer.jpg",
+  },
+];
